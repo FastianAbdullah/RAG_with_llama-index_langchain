2609baf01823a358672ffad18cbce75956efa251
M.Abdullah <93811691+FastianAbdullah@users.noreply.github.com>
2024-08-18 03:08:50 +0500

    deleted testing file

--- check.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-groq_api_key = st.secrets["GROQ_API_KEY"]
-user_agent = st.secrets["USER_AGENT"]
-
-print(f"GROQ_API_KEY: {groq_api_key}")
-print(f"USER_AGENT: {user_agent}")
